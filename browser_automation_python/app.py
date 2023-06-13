509da6088076dcbf44fde4066dc3dc3cf31fefa8
savhsergio <sergioandresherrera@gmail.com>
2023-06-12 21:21:32 -0500

    corrected

--- browser_automation_python/app.py
+++ browser_automation_python/app.py
@@ -36,21 +36,10 @@ options.add_experimental_option('detach', True)
 driver = webdriver.Chrome(options=options)
 
 
-def sign_in(driver=driver):
-    # opening the website
-    driver.get('https://robotsparebinindustries.com/')
-    # environment variables
-    robospare_user = os.environ.get('robospare_user')
-    robospare_pass = os.environ.get('robospare_pass')
 
-    driver.implicitly_wait(10)
 
-    # Indentify fields
-    username_field = driver.find_element(By.ID, 'username')
 
-    password_field = driver.find_element(By.ID, 'password')
 
-    login_button = driver.find_element(By.CLASS_NAME, 'btn-primary')
 
     driver.implicitly_wait(10)
     # enter info on fields
@@ -59,7 +48,6 @@ def sign_in(driver=driver):
     password_field.send_keys(str(robospare_pass))
     button_click = login_button.click()
 
-    driver.implicitly_wait(50)
 
 
 def create_directory():
