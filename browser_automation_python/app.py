7e801fa04cee2e3772ccddaba8c2a74f123617b7
savhsergio <sergioandresherrera@gmail.com>
2023-07-31 23:38:42 -0500

    screenshots for orders are working fully

--- browser_automation_python/app.py
+++ browser_automation_python/app.py
@@ -31,17 +31,16 @@ import pdfkit
 from plyer import notification
 
 options = Options()
-options.add_experimental_option('excludeSwitches',['enable-automation'])#remove the message of it being controlled by automation software
 
-options.add_argument('--disable-infobars')
-
-#options.add_argument('--disable-notifications')
-
+# remove the message of it being controlled by automation software
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+options.add_experimental_option('detach', True)
 
 
 # creating an  instance of the webdriver
 driver = webdriver.Chrome(options=options)
 driver.maximize_window()
+
 
 def sign_in(driver=driver):
     # opening the website
@@ -49,7 +48,7 @@ def sign_in(driver=driver):
     # environment variables
     robospare_user = os.environ.get('robospare_user')
     robospare_pass = os.environ.get('robospare_pass')
-    print(robospare_pass)
+    
 
     driver.implicitly_wait(10)
 
@@ -66,8 +65,6 @@ def sign_in(driver=driver):
     driver.implicitly_wait(10)
     password_field.send_keys(str(robospare_pass))
     button_click = login_button.click()
-
-    driver.implicitly_wait(50)
 
 
 def create_directory():
@@ -135,86 +132,104 @@ def sales_screenshot(driver=driver):
 
 
 def sales_to_pdf():
-    #base xpath for sales table
+    # base xpath for sales table
     sales_table = driver.find_element(
         By.XPATH, '//*[@id="sales-results"]/table')
-    
-    #xpath for table headers , it returns a list of webdriver elements
-    table_header = driver.find_elements(By.XPATH,'//*[@id="sales-results"]/table/thead/tr/th')
-    #xpath for name rows
+
+    # xpath for table headers , it returns a list of webdriver elements
+    table_header = driver.find_elements(
+        By.XPATH, '//*[@id="sales-results"]/table/thead/tr/th')
+    # xpath for name rows
     name_rows = driver.find_elements(
         By.XPATH, '//*[@id="sales-results"]/table/tbody/tr[position() mod 2!=0]/td[1]')
-    
-    #xpath for target
-    target_rows = driver.find_elements(By.XPATH, "// *[@id='sales-results']/table/tbody/tr/td[2]")
-    #regular_table_rows=
+
+    # xpath for target
+    target_rows = driver.find_elements(
+        By.XPATH, "// *[@id='sales-results']/table/tbody/tr/td[2]")
+    # regular_table_rows=
     result_rows = driver.find_elements(
         By.XPATH, "// *[@id='sales-results']/table/tbody/tr/td[3]")
     difference_rows = driver.find_elements(
         By.XPATH, "// *[@id='sales-results']/table/tbody/tr/td[4]")
 
-    
-    
-    performance_rows = driver.find_elements(By.XPATH,"//*[@id = 'sales-results']/table/tbody/tr/td/span[@class='performance']")
-    
-    
-    
-    #create a new empty list to store  text from webdriver elements in table_header
-    header_text=[]
-    #loop through the table_header and the performance_rows
-    name_texts=[]
+    performance_rows = driver.find_elements(
+        By.XPATH, "//*[@id = 'sales-results']/table/tbody/tr/td/span[@class='performance']")
+
+    # create a new empty list to store  text from webdriver elements in table_header
+    header_text = []
+    # loop through the table_header and the performance_rows
+    name_texts = []
     target_texts = []
-    results_texts=[]
-    difference_texts=[]
-    performance_texts=[]
-   
-    
-    for  header_title in table_header:
-        #extract text from  each individual webdriver elements in table_header
-        header_title_text=header_title.text
-        #append each text to the empty list table_header_text
+    results_texts = []
+    difference_texts = []
+    performance_texts = []
+
+    for header_title in table_header:
+        # extract text from  each individual webdriver elements in table_header
+        header_title_text = header_title.text
+        # append each text to the empty list table_header_text
         header_text.append(header_title_text)
-    
+
     for name in name_rows:
-        name_text= name.text
+        name_text = name.text
         name_texts.append(name_text)
-        
+
     for target_row in target_rows:
         target_text = (target_row.text)
         target_texts.append(target_text)
-    
+
     for result in result_rows:
         result_text = (result.text)
         results_texts.append(result_text)
-        
+
     for difference in difference_rows:
         difference_text = (difference.text)
         difference_texts.append(difference_text)
-        
+
     for performance_row in performance_rows:
-        performance_row_text=performance_row.text
+        performance_row_text = performance_row.text
         performance_texts.append(performance_row_text)
+
+    # create df
+    df = pd.DataFrame(columns=header_text)
+    df['Name'] = name_texts
+
+    # insert values into each column of the dataframe
+    df['Target'] = target_texts
+    df['Result'] = results_texts
+    df['Difference'] = difference_texts
+
+   # add new dataframe column Performance
+    df['Performance'] = performance_texts
     
+    #style the dataframe
+    
+    properties = {"border": "2px solid black", "color": "black", "font-size": "16px",'text-align':'center','align':'center'}
+    df_header_styles={
+        'selector':'th:not(.index_name)',
+        'props':[('border','5px,solid,black'),('background-color','green'), ('padding', 'auto')]
+    }
+    styled_df=df.style.set_properties(**properties).hide(axis='index').set_table_styles([df_header_styles])
+    
+    
+    #df.to_html('table.html',index=False,col_space=20)
+
+    #turn  df into html
+    styled_df.to_html('table.html',header=False,index=False,col_space=20,)
+    
+    css = """
+        table {
+          margin: 0 auto;
+        }
+    """
 
     
-    #create df
-    df=pd.DataFrame(columns=header_text)
-    df['Name']=name_texts
-    
-    #insert values into each column of the dataframe
-    df['Target'] = target_texts
-    df['Result'] =results_texts
-    df['Difference']=difference_texts
-    
-   #add new dataframe column Performance
-    df['Performance']=performance_texts
-        
-    #turn  df into html
-    df.to_html('table.html',classes='data', header="true", justify='center', index=False)
-    
-    #create pdf out of html file
-    pdfkit.from_file('table.html', 'table.pdf')
-    
+
+
+    # create pdf out of html file
+    pdfkit.from_file('table.html','table.pdf',)
+
+
 def download_orders_file():
 
     os.chdir('../orders')
@@ -226,7 +241,7 @@ def download_orders_file():
 
 
 def get_order_page(driver=driver):
-    driver.find_element(By.LINK_TEXT,'Order your robot!').click()
+    driver.find_element(By.LINK_TEXT, 'Order your robot!').click()
 
     # order_page = driver.find_element(By.XPATH, '//*[@id="root"]/header/div/ul/li[2]/a')
     # order_page.click()
@@ -274,44 +289,57 @@ def create_orders():
 
         legs_field.send_keys(legs)
         address_field.send_keys(address)
-        # preview robot
 
         preview_button.click()
         # show order
-        order_butotn = driver.find_element(By.ID, 'order')
+        order_button = driver.find_element(By.ID, 'order')
         order_button.click()
 
         # html receipt
-        receipt = driver.find_element(By.ID, 'receipt')
-        receipt_screenshot=receipt.screenshot('receipt_pic.png')
-        receipt_html = receipt.get_attribute('outerHTML')
-        with open('receipt.html', 'w') as f:
-            f.write(receipt_html)
-        # prepare for pdf
         
+       
+        receipt = driver.find_element(By.ID, 'receipt')   
+        wait = WebDriverWait(receipt, timeout=10, poll_frequency=5, ignored_exceptions=[
+            StaleElementReferenceException, NoSuchElementException])  
+        
+        receipt_screenshot = receipt.screenshot('receipt_pic.png')
+        #robot preview
+       
+        robots = driver.find_element(
+            By.XPATH, "//div[@id='robot-preview']")
+        wait = WebDriverWait(driver, timeout=10, poll_frequency=5, ignored_exceptions=[
+            StaleElementReferenceException, NoSuchElementException])  
+        robots.screenshot('robot_screenshot.png')
+        """
+        
+        # prepare for pdf
 
         # create pdf
-        #pdfkit.from_file('receipt.html', 'order.pdf')
+        # pdfkit.from_file('receipt.html', 'order.pdf')
 
         # screenshot
-        ordered_robots = driver.find_element(
-            By.XPATH, "//div[@id='robot-preview']").screenshot('robot_screenshot.png')
-        #driver.execute_script("document.body.style.zoom='49%'")
-        pdfkit.from_file(['receipt_pic.png','robot_screenshot.png'],'order-pdf.pdf'       )
-        
 
         
-        #driver.implicitly_wait(20)
 
-    #notification.notify(title='order completed',
-                       # message='excellent', timeout=10)
+        driver.execute_script("document.body.style.zoom='90%'")
+        ordered_robots.screenshot()
+        order_another_button=driver.find_element(By.ID, 'order-another')
+        order_another_button.click()
+        
+        pdfkit.from_file(
+            ['receipt_pic.png', 'robot_screenshot.png'], 'order-pdf.pdf')
+        wait = WebDriverWait(driver, timeout=10, poll_frequency=5, ignored_exceptions=[
+            StaleElementReferenceException, NoSuchElementException])
+       
+        
+
+    # notification.notify(title='order completed',
+        # message='excellent', timeout=10)
 
 
 def log_out(driver=driver):
     pass
-    
-    
-    
+"""
 
 sign_in()
 create_directory()
@@ -325,6 +353,7 @@ create_orders()
 
 """
 https://robotsparebinindustries.com/#/robot-order
+""""""
 
 Only the robot is allowed to get the orders file. You may not save the file manually on your computer.
 The robot should save each order HTML receipt as a PDF file.
